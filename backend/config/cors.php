ba2451823ea75e20f9ad41bd6eaaf986b3b63af6
issac-D <yishaqdamtew986@gmail.com>
2026-03-31 22:14:33 +0300

    ublish Sanctum migrations and migrate. Create routes/api.php. Restore a proper config/cors.php config array.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,5 +1,21 @@
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
+<?php
 
-'allowed_origins' => ['http://localhost:3000'],
+return [
 
-'supports_credentials' => true,
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
